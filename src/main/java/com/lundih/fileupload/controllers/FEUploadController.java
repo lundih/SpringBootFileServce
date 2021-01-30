9b0c5482091f15fddbd256b9eabcdc9368b95bb7
lundih[B <lundih27@gmail.com>
2021-01-30 16:15:36 +0300

    feat: Add basic front end to upload file

---
+++ src/main/java/com/lundih/fileupload/controllers/FEUploadController.java
@@ -0,0 +1,14 @@
+package com.lundih.fileupload.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class FEUploadController {
+
+    @GetMapping("/upload-files")
+    ModelAndView fileUpload() {
+        return new ModelAndView("index.html");
+    }
+}
